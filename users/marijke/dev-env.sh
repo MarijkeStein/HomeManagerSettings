038e78e27fdec8badd25548409de076bcc4840eb
Marijke Stein <marijke.stein@hfmdk-frankfurt.de>
2025-11-25 14:35:01 +0100

    polishing

--- users/marijke/dev-env.sh
+++ users/marijke/dev-env.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 pycharm-community &
 
-gnome-terminal --geometry 135x30+2100+1400 &
+# gnome-terminal --geometry 135x30+2100+1400 &
 
 signal-desktop &
 
@@ -18,12 +18,12 @@ nextcloud &
 
 sleep 1
 
+# Work log etc.
+kate ~/misc/log/current.txt ~/Notes.txt &
+
 firefox --new-window https://owa.rz.hfmdk-frankfurt.de/owa/#path=/calendar/view/WorkWeek \
                      https://fluffychat.im/web \
                      https://zeiterfassung.hfmdk-frankfurt.de &
-
-# Work log etc.
-kate ~/misc/log/current.txt ~/Notes.txt &
 
 
 # EOF
